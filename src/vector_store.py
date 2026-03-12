cfa7bfdd56a7f35fdb06e7c8fd0419e72ed9e6be
theHoodguy4587 <senitha02@gmail.com>
2026-03-12 22:54:19 +0530

    vectore store

---
+++ src/vector_store.py
@@ -0,0 +1,7 @@
+from langchain_community.vectorstores import FAISS
+from embedding_model import embeddings
+from data_loader import chunks
+
+vector_store = FAISS.from_documents(chunks, embeddings)
+
+retriever = vector_store.as_retriever( search_kwargs={"k": 3})
