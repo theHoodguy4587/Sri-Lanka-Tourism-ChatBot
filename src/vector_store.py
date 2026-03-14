18b9a34dadc1e79cf0a40eecafe248d1f3fd7858
theHoodguy4587 <senitha02@gmail.com>
2026-03-14 14:47:38 +0530

    vector store

--- src/vector_store.py
+++ src/vector_store.py
@@ -4,4 +4,4 @@ from data_loader import chunks
 
 vector_store = FAISS.from_documents(chunks, embeddings)
 
-retriever = vector_store.as_retriever( search_kwargs={"k": 3})
+retriever = vector_store.as_retriever( search_kwargs={"k": 8})
